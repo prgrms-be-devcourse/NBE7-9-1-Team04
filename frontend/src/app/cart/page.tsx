b6b0fa196b5b1f638a3b0922b1c07473a204fe4a
gandaraipower <jjangjongsoo@naver.com>
2025-09-23 15:27:58 +0900

    frontend 초기 설정

---
+++ frontend/src/app/cart/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">커피 CRUD cart 페이지입니다</h1>
+      {/* 여기에 앞으로 장바구니 관련 UI를 만들어 나가시면 됩니다. */}
+    </div>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Home() {
+  return <div>커피 CRUD 메인 페이지</div>;
+}
