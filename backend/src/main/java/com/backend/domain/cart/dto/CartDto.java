fe3cd48893269057c71e68937243cc4c98f281fb
dpwl0974 <dpwl0974@gmail.com>
2025-09-24 11:35:31 +0900

    feat: 장바구니 스웨거 생성

--- backend/src/main/java/com/backend/domain/cart/dto/CartDto.java
+++ backend/src/main/java/com/backend/domain/cart/dto/CartDto.java
@@ -1,4 +1,16 @@
 package com.backend.domain.cart.dto;
 
+import lombok.Getter;
+import java.time.LocalDateTime;
+
+@Getter
 public class CartDto {
+
+    private Long cartId;
+    private Long userId;
+    private Long menuId;
+    private int quantity;
+    private int orderAmount;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
