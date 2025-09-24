cb6c8e866732e7974f4da99eee851e95cfc8bc5a
dpwl0974 <dpwl0974@gmail.com>
2025-09-24 10:21:39 +0900

    feat: 장바구니 엔티티 생성

--- backend/src/main/java/com/backend/domain/cart/entity/Cart.java
+++ backend/src/main/java/com/backend/domain/cart/entity/Cart.java
@@ -1,4 +1,12 @@
 package com.backend.domain.cart.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Cart {
+    @Id
+    @GeneratedValue
+    private Long id;
 }
