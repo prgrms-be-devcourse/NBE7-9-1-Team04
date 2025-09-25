35883103eba540d5a0f69305518bbe9f2e7d53b2
dpwl0974 <dpwl0974@gmail.com>
2025-09-25 10:29:40 +0900

    feat: 장바구니 전체 조회 기능 구현

--- backend/src/main/java/com/backend/domain/cart/repository/CartRepository.java
+++ backend/src/main/java/com/backend/domain/cart/repository/CartRepository.java
@@ -4,8 +4,12 @@ import com.backend.domain.cart.entity.Cart;
 import com.backend.domain.menu.entity.Menu;
 import com.backend.domain.user.user.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Optional<Cart> findByUserAndMenu(Users user, Menu menu);
+
+    List<Cart> findByUser(Users user);
 }
